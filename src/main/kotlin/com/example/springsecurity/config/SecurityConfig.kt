a0cf02fc75c79eac3cdab65a65fcc60ad53fb8ed
iamlearning1 <d1612k@gmail.com>
2024-10-14 22:17:02 +0200

    add compromised password checker

--- src/main/kotlin/com/example/springsecurity/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/springsecurity/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.example.springsecurity.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.password.CompromisedPasswordChecker
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker
 
 @Configuration
 class SecurityConfig {
@@ -31,7 +33,7 @@ class SecurityConfig {
 
     @Bean
     fun userDetailsService(): UserDetailsService {
-        val user = User.withUsername("user").password("{bcrypt}\$2y\$10\$5/1OrGef3j2m93DYsSO0A.LSloCoTlgpEpEpJ9aS/7EXpOMn6JYqW").authorities("read").build()
+        val user = User.withUsername("user").password("{bcrypt}\$2y\$10\$9zPaiFzoL5yVLEeGmM37N.SlBbjnpCH7vQ8K4magXWXyYJ8Qy7qX6").authorities("read").build()
         val admin = User.withUsername("admin").password("{bcrypt}\$2y\$10$/zH2EkJqdDZiJYkMs8ioasOWTz/bk8TP24v2ka/wTc0fRc5UEbvFgS").authorities("admin").build()
 
         return InMemoryUserDetailsManager(user, admin)
@@ -40,5 +42,10 @@ class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+
+    @Bean
+    fun compromisedPasswordChecker(): CompromisedPasswordChecker {
+        return HaveIBeenPwnedRestApiPasswordChecker()
     }
 }
