2c639a4d570475eb2dbd6c66abd1aef9d4fa8e54
iamlearning1 <d1612k@gmail.com>
2024-10-14 21:57:08 +0200

    add users in memory db

--- src/main/kotlin/com/example/springsecurity/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/springsecurity/config/SecurityConfig.kt
@@ -3,6 +3,9 @@ package com.example.springsecurity.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -21,5 +24,13 @@ class SecurityConfig {
         http.httpBasic { }
 
         return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user = User.withUsername("user").password("{noop}user12345").authorities("read").build()
+        val admin = User.withUsername("admin").password("{noop}admin12345").authorities("admin").build()
+
+        return InMemoryUserDetailsManager(user, admin)
     }
 }
