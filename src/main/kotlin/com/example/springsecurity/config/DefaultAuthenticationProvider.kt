1d08410ea12caa4ae429ce919d4a2684b8e4d200
iamlearning1 <d1612k@gmail.com>
2024-10-17 22:22:49 +0200

    use different authentication provider based on profile

--- src/main/kotlin/com/example/springsecurity/config/DefaultAuthenticationProvider.kt
+++ src/main/kotlin/com/example/springsecurity/config/DefaultAuthenticationProvider.kt
@@ -2,37 +2,24 @@ package com.example.springsecurity.config
 
 import org.springframework.context.annotation.Profile
 import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.userdetails.UsernameNotFoundException
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
-@Profile("production")
-class AuthenticationProvider(
-    private val userDetailsService: UserDetailsService,
-    private val passwordEncoder: PasswordEncoder
+@Profile("default")
+class DefaultAuthenticationProvider (
+    private val userDetailsService: UserDetailsService
 ) : AuthenticationProvider {
 
     @Throws(AuthenticationException::class)
     override fun authenticate(authentication: Authentication?): Authentication {
-        val username = authentication?.name
-        val password = authentication?.credentials.toString()
-
-        if (username == null) throw UsernameNotFoundException("Username not provided");
-
+        val username = authentication?.name ?: throw UsernameNotFoundException("Username not provided")
         val userDetails = userDetailsService.loadUserByUsername(username)
 
-        val matches = passwordEncoder.matches(password, userDetails.password)
-
-        if (matches) {
-            return UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
-        }
-
-        throw BadCredentialsException("Something went wrong");
+        return UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
     }
 
     override fun supports(authentication: Class<*>?): Boolean {
