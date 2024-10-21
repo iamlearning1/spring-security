7edd2d86558fdc15e6e3a4694b47cddab51574bd
iamlearning1 <d1612k@gmail.com>
2024-10-21 17:51:00 +0200

    add roles to route protection

--- src/main/kotlin/com/example/springsecurity/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/springsecurity/config/SecurityConfig.kt
@@ -7,8 +7,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler
 
 @Configuration
 class SecurityConfig {
@@ -16,31 +14,18 @@ class SecurityConfig {
     @Bean
     @Throws(Exception::class)
     fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        // enables https protocol for all requests
-        // http.requiresChannel { channelHandler -> channelHandler.anyRequest().requiresSecure() }
-
-        val csrfTokenRequestAttributeHandler = CsrfTokenRequestAttributeHandler()
-
         http.authorizeHttpRequests {
             requests -> requests
                 .requestMatchers("/accounts/**")
-                .authenticated()
-                .requestMatchers("/notices", "/error", "/users/**", "/invalid-session")
+                .hasAnyRole("ADMIN", "USER")
+                .requestMatchers("/notices", "/error", "/users/**")
                 .permitAll()
         }
 
         http.formLogin { it.disable() }
-        http.csrf {
-            it
-                .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringRequestMatchers("/notices", "/error")
-        }
+        http.csrf { it.disable() }
+
         http.httpBasic { it.authenticationEntryPoint(CustomBasicAuthenticationEntryPoint()) }
-
-        http.addFilterAfter(CsrfCookieFilter(), AnonymousAuthenticationFilter::class.java)
-
-        http.sessionManagement { it.invalidSessionUrl("/invalid-session") }
 
         return http.build()
     }
